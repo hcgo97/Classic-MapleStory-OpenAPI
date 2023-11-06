dfebd0e0c1cd02c5d9a97ad54cc3f0c92c272e1b
hcgo97 <hcgo_o@naver.com>
2023-11-06 22:35:40 +0900

    ✨project init

---
+++ src/main/java/me/hyoj/kms/ClassicMaplestoryOpenapiApplication.java
@@ -0,0 +1,13 @@
+package me.hyoj.kms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicMaplestoryOpenapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassicMaplestoryOpenapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hyoj/kms/ClassicMaplestoryOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyoj.kms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicMaplestoryOpenapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
